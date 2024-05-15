cf94b31f6fc370824d2a18e97d092e5761a715b7
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2024-05-15 07:36:27 +0530

    Create convert a given string into lowercase.cs

---
+++ Basics/Tutorial/convert a given string into lowercase.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutoq10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Write a C# Sharp Program to display the following pattern using the alphabet.";
+            Console.WriteLine(line.ToLower());
+        }
+    }
+}
